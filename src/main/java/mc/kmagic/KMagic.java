7154b7b20109dc2765e37c946715e99b9666b6cd
boyutov <minatomn654432@gmail.com>
2026-03-29 13:21:00 +0600

    first commit

---
+++ src/main/java/mc/kmagic/KMagic.java
@@ -0,0 +1,19 @@
+package mc.kmagic;
+
+import mc.kmagic.item.ModItemGroups;
+import mc.kmagic.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KMagic implements ModInitializer {
+	public static final String MOD_ID = "kmagic";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
+	}
+}
